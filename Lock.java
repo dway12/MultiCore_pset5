b21eb24dc07b0c67a304d02227c5fbcdf8bcee64
David H Way <dway@mit.edu>
2012-11-08 01:05:04 -0500

    commiting new files

---
+++ Lock.java
@@ -0,0 +1,123 @@
+import java.util.*;
+import java.util.concurrent.locks.*;
+
+interface ReadWriteLock {
+  Lock readLock();
+  Lock writeLock();
+}
+
+interface Lock {
+  void lock();
+  //Condition newCondition();
+  void unlock();
+}
+
+
+class FIFOReadWriteLock implements ReadWriteLock {
+  int readAcquires, readReleases;
+  boolean writer;
+  ReentrantLock lock;
+  Condition condition;
+  Lock readLock, writeLock;
+  public FIFOReadWriteLock() {
+    writer = false;
+    readAcquires = readReleases = 0;
+    lock = new ReentrantLock();
+    readLock = new ReadLock();
+    writeLock = new WriteLock();
+    condition = lock.newCondition();
+  }
+  public Lock readLock() {
+    return readLock;
+  }
+  public Lock writeLock() {
+    return writeLock;
+  }
+
+  private class ReadLock implements Lock {
+    public void lock() {
+      lock.lock();
+      try {
+        while (writer) {
+          condition.await();
+        }
+        readAcquires++;
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }finally {
+        lock.unlock();
+      }
+    }
+    public void unlock() {
+      lock.lock();
+      try {
+        readReleases++;
+        if (readAcquires == readReleases)
+          condition.signalAll();
+      } finally {
+        lock.unlock();
+      }
+    }
+  }
+  private class WriteLock implements Lock {
+    public void lock() {
+      lock.lock();
+      try {
+        while (writer) {
+          condition.await();
+        }
+        writer = true;
+        while (readAcquires != readReleases) {
+          condition.await();
+        }
+      } catch (InterruptedException e) {
+        e.printStackTrace(); 
+      } finally {
+        lock.unlock();
+      }
+    }
+    public void unlock() {
+      writer =false;
+      condition.signalAll();
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
